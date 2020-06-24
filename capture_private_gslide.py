6270ac50f19b5e03aa59b5f9ffbace48549ffb3a
kobrr <koba.csis@gmail.com>
2020-06-25 06:43:25 +0900

    fix start part and go-next-page part

--- capture_private_gslide.py
+++ capture_private_gslide.py
@@ -10,8 +10,10 @@ from selenium.webdriver.common.by import By
 # avoid SessionNotCreatedException
 from webdriver_manager.chrome import ChromeDriverManager
 
-def capture_gslide(chromedriver_path, gslide_url, save_folder):
+
+def capture_gslide(chromedriver_path, gslide_url, max_page_no, save_folder=''):
     """ take screenshots of Google Slide
+        max_page_no: int
     """
     # avoid SessionNotCreatedException
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -24,34 +26,42 @@ def capture_gslide(chromedriver_path, gslide_url, save_folder):
     actions.perform()
     time.sleep(3)
 
-    # get the number of slides
-    contents = driver.find_elements_by_xpath('.//*[@id="filmstrip"]')
-    lst = [i.text for i in contents][0].split('\n')
-    lst = [int(i) for i in lst if re.match(r'[0-9]+', i)]
-    max_pages = max(lst)
-    print('the number of slides: ', max_pages)
-    time.sleep(3)
+    # TODO:
+    # get the number of slides ← different btwn each slide
+    # contents = driver.find_elements_by_xpath('.//*[@id="filmstrip"]')
+    # lst = [i.text for i in contents][0].split('\n')
+    # lst = [int(i) for i in lst if re.match(r'[0-9]+', i)]
+    # max_page_no = max(lst)
+    # print('the number of slides: ', max_page_no)
+    # time.sleep(3)
 
     # click on Present button and start Present from 1st slide
-    present_pulldown =  driver.find_element_by_xpath('.//*[@id="punch-start-presentation-right"]')
-    present_pulldown.click()
-    time.sleep(3)
-    start_present =  driver.find_element_by_xpath('.//*[@id=":31"]')
-    start_present.click()
-    time.sleep(10)
+    # present_pulldown =  driver.find_element_by_xpath('.//*[@id="punch-start-presentation-right"]')
+    # present_pulldown.click()
+    # time.sleep(3)
+
+    # start the presentation
+    keys = Keys()
+    actions = ActionChains(driver)
+    actions.send_keys(keys.COMMAND + keys.SHIFT + keys.ENTER)
+    actions.key_down(keys.SHIFT).key_down(keys.COMMAND).send_keys(keys.ENTER)
+    actions.perform()
+    time.sleep(8)
 
     # move to next page until the end
+    # pressing PAGE_DOWN or ENTER seems not to work
     actions = ActionChains(driver)
-    actions.send_keys(Keys.PAGE_DOWN)
+    actions.click()
     counter = 1
-    while counter < max_pages:
-        counter += 1
+    while counter <= int(max_page_no):
         fname = '{}.png'.format(str(counter).zfill(2))
         save_dir = os.path.join(save_folder, fname)
         driver.save_screenshot(save_dir)
         print('Page ' + str(counter) + ' was captured.')
-        time.sleep(2)
         actions.perform()
+        counter += 1
+        time.sleep(2)
+
 
     print('DONE')
 
